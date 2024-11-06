6783dc2072103dc0598ddda22e478fc89d1f2085
Tixxy-A <anujraut7221@gmail.com>
2024-11-06 13:06:30 +0530

    final touches

--- src/component/About.js
+++ src/component/About.js
@@ -1,22 +1,27 @@
 import about from "./about.jpg";
+import {motion} from "framer-motion"
 import { ABOUT_TEXT } from "./Data";
+
 function About() {
   return (
     <div className="mt-10 border-b border-neutral-800 pb-8">
-      <h1 className="my-20 text-4xl text-center">
+      <motion.h1 whileInView={{y:0,opacity:1}}  initial={{y:100,opacity:0}} transition={{duration:0.5,delay:0.5}} 
+      className="my-20 text-4xl text-center">
         About <span className="text-gray-500">Me</span>
-      </h1>
+      </motion.h1>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2">
+        <motion.div whileInView={{x:0,opacity:1}}  initial={{x:-100,opacity:0}} transition={{duration:0.5,delay:0.5}}
+         className="w-full lg:w-1/2">
           <div className="flex items-center justify-center">
             <img src={about} className="rounded-lg size-80" alt="fuvk" />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div  whileInView={{x:0,opacity:1}}  initial={{x:100,opacity:0}} transition={{duration:0.5,delay:0.5}}
+        className="w-full lg:w-1/2">
           <div className="flex lg:justify-start justify-center">
             <p className="my-2 py-6 max-w-xl">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/component/Data.js
+++ src/component/Data.js
@@ -3,39 +3,39 @@ import football from './football.png'
 import gifs from './gifs.png'
 import hotel from './hotel.png'
 
-export const hero_description="I am a seasoned full stack developer with a passion for creating scalable and efficient web applications. With a robust technical stack that includes ReactJS, NodeJS, Spring Boot, MongoDB, and MySQL, I excel in both front-end and back-end development.With a keen eye for detail and a commitment to excellence, I focus on delivering innovative solutions that not only meet but exceed client expectations. My goal is to provide exceptional user experiences through intuitive and responsive design, seamless functionality, and reliable performance. I can tackle complex challenges and deliver high-quality results in every project I undertake.";
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const hero_description="I am a full stack developer with a passion for creating scalable and efficient web applications. With a robust technical stack that includes ReactJS, NodeJS, Spring Boot, MongoDB, and MySQL, I excel in both front-end and back-end development.With a keen eye for detail and a commitment to excellence, I focus on delivering innovative solutions that not only meet but exceed client expectations. My goal is to provide exceptional user experiences through intuitive and responsive design, seamless functionality, and reliable performance. I can tackle complex challenges and deliver high-quality results in every project I undertake.";
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 export const PROJECTS = [
     {
-      title: "E-Commerce Website",
+      title: "Cloud Broker Website",
       image: cloud,
       link: 'https://660c1746a0e0f10008ef5e49--cloud-brooker.netlify.app/landing',
       description:
-        "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+        "A platform where cloud providers list services, and customers can filter and purchase based on their needs. Features include service filtering, user authentication, and a streamlined user interface.",
       technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
     },
     {
-      title: "Task Management App",
+      title: "Fun football Game",
       link: 'https://6606b6c9f55c270008ac2328--1kgoalchallenge.netlify.app/',
       image: football,
       description:
-        "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-      technologies: ["HTML", "CSS", "Angular", "Firebase"],
+        "A fun, interactive game that tests players' football knowledge with timed questions and score tracking.",
+      technologies: ["HTML", "Redux", "React", "Tailwind CSS"],
     },
     {
-      title: "Portfolio Website",
+      title: "Hotel booking Application Website",
       image: hotel,
       link: 'https://65f82d643123f0000867a940--aiaabnb.netlify.app/',
       description:
-        "A personal portfolio website showcasing projects, skills, and contact information.",
-      technologies: ["HTML", "CSS", "React", "Bootstrap"],
+        "An application for searching, filtering, and booking hotels. Features include real-time availability, room details, and secure payment integration for a seamless booking experience.",
+      technologies: ["HTML", "CSS", "React", "Bootstrap", "NodeJs", "MongoDB"],
     },
     {
-      title: "Blogging Platform",
+      title: "Gifify",
       image: gifs,
       link: 'https://669a515a70a3843cc7bb4aa1--unlimitedfun.netlify.app/',
       description:
-        "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+        "A GIF and sticker browsing app that lets users explore a vast library of GIFs and stickers across categories. Features include quick search, trending collections, and easy sharing options. ",
       technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
     },
   ];
